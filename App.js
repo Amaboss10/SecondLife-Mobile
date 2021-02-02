b965978b80b3991a62279384b4df2889a7900d3b
Dark-Folt <marigo9794@gmail.com>
2021-02-02 20:33:54 +0100

    refactorisation du code

--- App.js
+++ App.js
@@ -1,94 +1,13 @@
 import * as React from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { Button } from 'native-base';
+import BottomNavigation from './components/BottomNavigation';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-      <Text>Home!</Text>
-      {/* <Button light style={{ alignItems: 'center' }}><Text>This is home!</Text></Button> */}
-    </View>
- 
-  );
-}
-
-function ComptesScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Favoris!</Text>
-    </View>
-  );
-}
-
-function FavorisScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Favoris!</Text>
-    </View>
-  );
-}
-
-function AjoutScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Ajouter article !</Text>
-    </View>
-  );
-}
-
-function CategorieScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-      screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Favoris') {
-              iconName = focused ? 'heart-circle' : 'heart-circle-outline';
-            }
-            else if (route.name === 'Annonce') {
-              iconName = focused ? 'add-circle' : 'add-circle-outline';
-            }
-            else if (route.name === 'Compte') {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-            else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-list' : 'ios-list';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Favoris" component={FavorisScreen} />
-        <Tab.Screen name="Annonce" component={AjoutScreen}        />
-        <Tab.Screen name="Compte" component={ComptesScreen} />
-        <Tab.Screen name="Categorie" component={CategorieScreen} />
-      </Tab.Navigator>
+      <BottomNavigation/>
     </NavigationContainer>
   );
 }
