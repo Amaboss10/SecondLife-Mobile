c7a8793d108e5e9b87c56248463eb63679ca4398
Dark-Folt <marigo9794@gmail.com>
2021-02-09 15:13:19 +0100

    ajout du safeArea pour les smarphone apple( Iphone X)

--- sreens/AddAnnonce.js
+++ sreens/AddAnnonce.js
@@ -1,118 +1,122 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, TextInput, StyleSheet, Style, Button} from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, StyleSheet, Style, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import Home  from './Home.js'
+import Home from './Home.js'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
-function _onCancel(){
-    return this.Home = () => {}
+function _onCancel() {
+  return this.Home = () => { }
 }
 
 const AddAnnonce = () => {
-    return (
-        <ScrollView>
-        <View  style={{flexDirection: 'row'}}>
-        <Button style={styles.Text} title= "Annuler" onPress={() => this._onCancel() } />
-        <Text style={styles.Text1}>Ajoutez une annonce </Text>
+  return (
+    <SafeAreaView>
+
+      <ScrollView>
+        <View style={{ flexDirection: 'row' }}>
+          <Button style={styles.Text} title="Annuler" onPress={() => this._onCancel()} />
+          <Text style={styles.Text1}>Ajoutez une annonce </Text>
         </View>
 
 
         <View style={styles.main_container}>
-        <Text style={styles.title_text}>Titre de l'annonce </Text>
-        <Image
-          style={styles.image}
-          source={{uri: ""}}
-        />
-        <View style={styles.content_container}>
-          <View style={styles.header_container}>
-            <TextInput style={styles.title_text1} placeholder= "Titre de l'Annonce" />
+          <Text style={styles.title_text}>Titre de l'annonce </Text>
+          <Image
+            style={styles.image}
+            source={{ uri: "" }}
+          />
+          <View style={styles.content_container}>
+            <View style={styles.header_container}>
+              <TextInput style={styles.title_text1} placeholder="Titre de l'Annonce" />
             </View>
             <View style={styles.header_container2}>
-            <TextInput style={styles.title_text2} placeholder= "Prix" />
+              <TextInput style={styles.title_text2} placeholder="Prix" />
             </View>
             <View style={styles.header_container}>
-            <TextInput style={styles.title_text1} placeholder= "Catégorie" />
-            <TextInput style={styles.title_text1} placeholder= "Ref" />
-            <TextInput style={styles.title_text1} placeholder= "Lieu/Date" />
+              <TextInput style={styles.title_text1} placeholder="Catégorie" />
+              <TextInput style={styles.title_text1} placeholder="Ref" />
+              <TextInput style={styles.title_text1} placeholder="Lieu/Date" />
+            </View>
+
           </View>
-          
         </View>
-      </View>
-        </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-  
 
 
-   
-    
+
+
+
 const styles = StyleSheet.create({
   main_container: {
     height: 190,
-   // flexDirection: 'row',
+    // flexDirection: 'row',
     margin: 10,
   },
-   content_container: {
+  content_container: {
     flex: 1,
     margin: 5
   },
   image: {
     width: 150,
     height: 100,
-    alignItems:'center',
-    justifyContent:'flex-start',
-    backgroundColor: 'gray', 
-    flex:1
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: 'gray',
+    flex: 1
   },
   header_container: {
-    flex:7
+    flex: 7
   },
   header_container2: {
-    flex:7,
-    flexDirection:'row',
-    alignItems:'flex-end',
-    justifyContent:'flex-end'
+    flex: 7,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end'
   },
   title_text: {
     fontWeight: 'bold',
     fontSize: 17,
-    margin:-1 ,
+    margin: -1,
     flex: 1
   },
   title_text1: {
     fontWeight: 'bold',
     fontSize: 13,
-   flexWrap: 'wrap',
+    flexWrap: 'wrap',
     paddingRight: 5,
   },
   title_text2: {
     fontWeight: 'bold',
     fontSize: 13,
-   flexWrap: 'wrap',
+    flexWrap: 'wrap',
     paddingRight: 5,
     flexDirection: 'row',
-    alignItems:'flex-end'
+    alignItems: 'flex-end'
   },
   Styles: {
-       width: 113, 
-       height: 60, 
-       justifyContent: 'flex-start', 
-       margin:17 
-       },
-    Text: {
-      alignItems:'flex-start',
-      justifyContent: 'flex-start',
-      
+    width: 113,
+    height: 60,
+    justifyContent: 'flex-start',
+    margin: 17
+  },
+  Text: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
 
-    },
-    Text1: {
-      alignItems:'flex-end',
-      justifyContent: 'flex-end',
-      paddingLeft: 55,
-      fontSize: 20,
-      fontWeight: 'bold'
 
-    }
+  },
+  Text1: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    paddingLeft: 55,
+    fontSize: 20,
+    fontWeight: 'bold'
+
+  }
 });
 
 
