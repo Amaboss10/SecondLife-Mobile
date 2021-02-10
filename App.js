50ca6eab58d98bbe83ef860751c744296ead8fb2
Dark-Folt <marigo9794@gmail.com>
2021-02-10 23:35:42 +0100

    code review

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import BottomNavigation from './components/BottomNavigation';
 import { NavigationContainer } from '@react-navigation/native';
-// import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
 import StackNavigation from './components/StackNavigation';
 
 const Stack = createStackNavigator();
