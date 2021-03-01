878ad197c6296f9f92e9eab6807e5fe7af8ae815
Dark-Folt <34268306+Dark-Folt@users.noreply.github.com>
2021-03-02 00:45:11 +0100

    creation du caroussel jpou l'affichage des annonces ainsi que la stylisation

--- screens/categories/Smartphone.js
+++ screens/categories/Smartphone.js
@@ -1,17 +1,67 @@
-import { Text, View } from 'native-base'
-import React from 'react'
+import axios from 'axios'
+import { Item, Text, View } from 'native-base'
+import React, { useState, useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import Carousel from 'react-native-snap-carousel'
+import { BASE_URL } from '../../assets/constantes'
+import ArticleItem from '../../components/Item'
 
 
 const Smartphone = () => {
+    const [activeIndex, setActiveIndex] = useState(0)
+    const [carouselItems, setCarouselItems] = useState([])
+
+    const loadItems = () => {
+        axios({
+            method: 'get',
+            url: BASE_URL + '/api/annonces',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/ld+json'
+            }
+        }).then((response) => {
+            //on met les données dans le carouselItems
+            setCarouselItems(response.data["hydra:member"])
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
+
+
+    useEffect(() => {
+        loadItems()
+    }, [])
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text>Test screen</Text>
+        <SafeAreaView style={localStyles.safeA} >
+            <View style={localStyles.container}>
+                <Carousel
+                    layout={"default"}
+                    data={carouselItems}
+                    sliderWidth={300}
+                    itemWidth={380}
+                    renderItem={({ item }) => <ArticleItem result={item} />}
+                    onSnapToItem={index => (setActiveIndex(index))}
+                />
             </View>
         </SafeAreaView>
     )
 }
 
+
+
 export default Smartphone
+
+const localStyles = {
+    safeA: {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        backgroundColor: 'white'
+    }
+
+}
