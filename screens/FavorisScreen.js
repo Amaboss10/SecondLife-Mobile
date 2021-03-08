369b39f05a2c00430a0c2e483d7e362e7c4fe696
ismail <ismailbrahimi222000@gmail.com>
2021-03-08 18:34:52 +0100

    update favoris

--- screens/FavorisScreen.js
+++ screens/FavorisScreen.js
@@ -1,88 +1,264 @@
-// import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import React from 'react';
-import { Image } from 'react-native';
-//import { Button } from 'native-base';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image,StyleSheet,ActivityIndicator , FlatList, SafeAreaView,Text, View , ScrollView , TextInput} from 'react-native';
+import React , { useState }from "react";
+import { Ionicons as Icon } from '@expo/vector-icons';
+import { createStackNavigator } from '@react-navigation/stack';
 import { SearchBar } from "react-native-elements";
-// import { Ionicons } from '@expo/vector-icons';
 import { SliderBox } from "react-native-image-slider-box";
 import { LinearGradient } from 'expo-linear-gradient';
 import { Divider } from "react-native-elements";
+import { Card } from "react-native-elements";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+//import { OptionsButton } from 'react-native-options-button'
+import { List, Button } from 'react-native-paper';
+import RNPickerSelect from 'react-native-picker-select';
+import ModalDropdown from 'react-native-modal-dropdown';
+import { Video, AVPlaybackStatus } from 'expo-av';
+import { Textarea } from 'native-base';
+import '../global'
+import { MON_IP } from '../assets/constantes';
+
+//-----------------------------------------
+const getUsersFromApi = () => {
+  return fetch('http://10.212.156.25:3000/annonce')
+    .then((response) => response.json())
+    .then((json) => {
+      global.annonce = json;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+getUsersFromApi()
+//-----------------------------------------
+
+export class FavorisScreen extends React.Component {
+  state = {
+    loading: true,
+    // navigation : navigation,
+    person: null
+  };
+  createTable = () => {
+    let table = []
+
+    // Outer loop to create parent
+    for (let i = 0; i < 3; i++) {
+      const data = [1]
+      let children = []
+      //Inner loop to create children
+
+    const { navigation,route } = this.props;
+      children.push(<FlatList 
+        data={data}
+        renderItem ={(item) =>   
+        <View style={{ borderRadius: 5, borderWidth: 1 , margin: 5, borderColor: '#e0e0e0' }} >
+                 <View style={{flexDirection:'row'}}>
+                <TouchableOpacity onPress={
+                    // () => alert(i)
+                    () => navigation.navigate('Annonce', { A: i })
+                  }>
+                
+                
+                 <Image
+              style={{ width: 160, height: 190 }}
+              resizeMode='cover'
+              source={{
+                uri:
+                  "https://i.ebayimg.com/images/g/1YgAAOSwy~JfzAGN/s-l300.jpg"
+              }}
+            />
+                 </TouchableOpacity>
+                  <List.Item              
+                  title={""}
+                  onPress={
+                    () => finduser(i),
+                    () => navigation.navigate('Annonce', { A: i })
+                    
+                  }
+                  left={props =>
+                  <View style={{ justifyContent: 'center',
+                  alignItems: 'center' }}> 
+                 
+                  </View>
+                }
+                right={props =>
+                  <View>
+                    <View style={{}}>
+                      <View style={{paddingLeft:95}}>
+                    <ModalDropdown  options={['Consulter', 'Contacter','Signaler']}>
+             
+             <View style={styles.quizAttrRight}>
+               <View style={{paddingLeft:100}}>
+                 <Icon name="ios-list" style={styles.infoIcon} size={20} />
+                 <Text style={styles.infoText}>{item.NumberQuestions}</Text>
+               </View>
+             </View>
+            
+            </ModalDropdown>
+            </View>
+                      <View style={{paddingBottom:0}}>
+                    <Text style={{fontSize:20 , fontWeight:'500'}}>
+                    {global.annonce[i].titre_annonce}
+                    </Text>
+                    </View>
+                    <Text style={{textAlign:'left' , color:'red'}}>
+                    {global.annonce[i].prix_annonce} EUR
+                    </Text>
+                    <Text>
+                    Catégorie : {global.annonce[i].categorie}
+                    </Text>
+                    <Text>
+                      Marque : {global.annonce[i].marque} 
+                    </Text>
+                    </View>
+                    <Button style={{ marginTop: 38 ,alignItems:'center' ,paddingLeft:10 , alignContent:'center'  , height:35 , justifyContent:'center'  }}
+                  color="tomato"
+                   
+                  mode='outlined' 
+                  onPress={() => alert("favoris")}>
+                    Retirer
+                  </Button>
+                  
+                  </View>
+                }
+              />
 
 
+            </View>
+
+          </View>
+        }
+      />)
 
 
+      table.push(<View style={styles.container}>{children}</View>)
+    }
+    return table
+  }
 
-const img = {
-  images: [
-    'https://media.discordapp.net/attachments/697960252011970600/806052472250302484/Publicite.png?width=1191&height=670',
-    'https://media.discordapp.net/attachments/697960252011970600/806053134199291914/Publicite_1.png?width=1191&height=670',
-    'https://media.discordapp.net/attachments/697960252011970600/806053377254752286/Publicite_2.png?width=1191&height=670'
-  ]
+  async componentDidMount() {
+
+    // mettez  votre adresse IP
+    const url = "http://10.212.156.25:3000/utilisateur";
+
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data, loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator size="large" color="tomato" />
+        </View>
+      );
+    }
+
+    if (!this.state.person) {
+      return <Text>Pas de favoris pour le moment :(</Text>;
+    }
+    const { navigation,route } = this.props;
+    return (
+
+      <View style={styles.container}>
+
+
+        <LinearGradient
+          colors={['#ffffff', '#c6f6ff', '#ff6347']}
+          style={styles.background}
+        >
+
+          <View >
+            <View style={{ alignItems: 'center', justifyContent: 'center', alignItems: 'center' }}>
+              <Image
+                source={{ uri: 'https://media.discordapp.net/attachments/697960252011970600/805578022891421706/Logo_Khbur_cmp.png' }}
+                style={{ width: 113, height: 60, justifyContent: 'center', margin: 17 }}
+              />
+            </View>
+
+
+            <View style={{ flexDirection: 'row' }}>
+              <View style={{ margin: -13, marginLeft: 1, width: 375, padding: -29, justifyContent: 'center' }}>
+                <SearchBar
+                  platform="android"
+                  containerStyle={{ paddingBottom: -10, paddingTop: -10 }}
+                  inputContainerStyle={{}}
+                  inputStyle={{}}
+                  leftIconContainerStyle={{}}
+                  rightIconContainerStyle={{}}
+                  loadingProps={{}}
+                  onChangeText={newVal => setValue(newVal)}
+                  onClearText={() => console.log(onClearText())}
+                  placeholder="Rechercher ici"
+                  placeholderTextColor="#888"
+                  cancelButtonTitle="Cancel"
+                  cancelButtonProps={{}}
+                  onCancel={() => console.log(onCancel())}
+                //value={value}
+                />
+              </View>
+              <TouchableOpacity onPress={() => navigation.navigate('FAQ / Aide')}>
+                <View style={{ paddingLeft: 15 }}>
+                  <Icon name="help-circle" style={styles.infoIcon} size={30} />
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={{ textAlign: 'left', marginTop: 10, paddingLeft: 10, height: 7 }}>
+
+            </View>
+          </View>
+          <ScrollView>
+            <View>
+
+            </View>
+
+
+            <View style={{ flexDirection: 'row' }}>
+              <View style={{ textAlign: 'left', marginTop: 10, paddingLeft: 10 }}>
+                <Text style={{ color: '#000000', fontWeight: '500', fontSize: 16 }} >Les Favoris :</Text>
+
+              </View>
+
+              
+            </View>
+
+            <Divider style={{ width: "80%", margin: 20 }} />
+
+            <View style={{ flex: 1, paddingTop: 5, backgroundColor: 'rgba(242, 250, 250, 42)' }}>
+
+              {this.createTable()}
+            </View>
+          </ScrollView>
+          <View style={{ height: 177 }}>
+            {/* navbar  */}
+          </View>
+        </LinearGradient>
+      </View>
+
+    );
+  }
 }
 
+////////////////////////////////////////////////////////////////////////////////
 
 
-const FavorisScreen = () => {
-  const [value, setValue] = React.useState("");
+
+const Stack = createStackNavigator();
+
+function Fave() {
   return (
-    <View>
-      <LinearGradient
-        colors={['#ffffff', '#c6f6ff', '#ff6347']}
-        style={styles.background}
-      >
-        <View style={{}}>
-          <View style={{ alignItems: 'center', justifyContent: 'center', alignItems: 'center' }}>
-            <Image
-              source={{ uri: 'https://media.discordapp.net/attachments/697960252011970600/805578022891421706/Logo_Khbur_cmp.png' }}
-              style={{ width: 113, height: 60, justifyContent: 'center', margin: 17 }}
-            />
-          </View>
-          <View style={{ margin: -13, marginLeft: 1, padding: -29 }}>
-            <SearchBar
-              platform="android"
-              containerStyle={{ paddingBottom: -10, paddingTop: -10 }}
-              inputContainerStyle={{}}
-              inputStyle={{}}
-              leftIconContainerStyle={{}}
-              rightIconContainerStyle={{}}
-              loadingProps={{}}
-              onChangeText={newVal => setValue(newVal)}
-              onClearText={() => console.log(onClearText())}
-              placeholder="Rechercher ici"
-              placeholderTextColor="#888"
-              cancelButtonTitle="Cancel"
-              cancelButtonProps={{}}
-              onCancel={() => console.log()}
-              value={value}
-            />
-          </View>
-          <View style={{ textAlign: 'left', margin: 20 }}>
-            <Text style={{ color: '#000000', fontWeight: '500', fontSize: 16 }}>Les Favoris :</Text>
-          </View>
-        </View>
-        <View>
-          <View>
-            <SliderBox
-              images={img.images}
-              onCurrentImagePressed={index =>
-                console.warn(`image ${index} pressed`)
-              }
-            />
-          </View>
-        </View>
-        <Divider style={{ width: "80%", margin: 20 }} />
-      </LinearGradient>
-    </View>
-  )
+
+    <Stack.Navigator initialRouteName="Fave">
+      <Stack.Screen name="Favoris" component={FavorisScreen} /> 
+    </Stack.Navigator>
+  );
 }
 
+export default Fave;
 
 const styles = StyleSheet.create({
   container: {
+    height: '100%',
     flex: 1,
 
   },
@@ -102,14 +278,96 @@ const styles = StyleSheet.create({
     fontWeight: "bold"
   },
   background: {
-    position: 'absolute',
+    position: 'relative',
     left: 0,
     right: 0,
     top: 0,
     height: 800,
 
   },
+  quizAttrContent: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    borderTopWidth: 1,
+    borderTopColor: '#e0e0e0',
+    height: 25,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    margin: 5,
+    paddingTop: 3
+  },
+  quizAttrLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 2
+  },
+  quizAttrMid: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  quizAttrRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 2,
+  },
+  infoText: {
+    color: '#676767',
+    fontSize: 25,
+
+  },
+  infoIcon: {
+    color: "#676767",
+    marginRight: 5,
+
+  },
+  input: {
+    height: 35,
+    width: 400,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'gray',
+    marginTop: 5,
+    backgroundColor: 'white',
+    paddingLeft: 10
+  },
+  inputmess: {
+    height: 85,
+    width: 400,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'gray',
+    marginTop: 5,
+    backgroundColor: 'white',
+    paddingLeft: 10
+  },
+  
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 10
+  },
+
 });
 
-export default FavorisScreen;
 
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingTop: 13,
+    paddingHorizontal: 10,
+    paddingBottom: 12,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    backgroundColor: 'white',
+    color: 'black',
+    width: 50,
+    height: 10,
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -19,7 +19,7 @@ import { MON_IP } from '../assets/constantes';
 
 //-----------------------------------------
 const getUsersFromApi = () => {
-  return fetch('http://10.189.116.41:3000/annonce')
+  return fetch('http://10.212.156.25:3000/annonce')
     .then((response) => response.json())
     .then((json) => {
       global.annonce = json;
@@ -99,12 +99,6 @@ export class HomeScreen extends React.Component {
                     <Text style={{fontSize:20 , fontWeight:'500'}}>
                     {global.annonce[i].titre_annonce}
                     </Text>
-                      <Text>
-                        Catégorie : {global.users[i].categorie}
-                      </Text>
-                      <Text>
-                        Marque : {global.users[i].marque}
-                      </Text>
                     </View>
                     <Text style={{textAlign:'left' , color:'red'}}>
                     {global.annonce[i].prix_annonce} EUR
@@ -135,7 +129,7 @@ export class HomeScreen extends React.Component {
   async componentDidMount() {
 
     // mettez  votre adresse IP
-    const url = "http://10.189.116.41:3000/utilisateur";
+    const url = "http://10.212.156.25:3000/utilisateur";
 
     const response = await fetch(url);
     const data = await response.json();
@@ -261,169 +255,7 @@ export class HomeScreen extends React.Component {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//DETAILS ANNONCE
 
-export class DetailsAnnonce extends React.Component {
-  state = {
-    loading: true,
-    // navigation : navigation,
-    person: null
-  };
-  createTable = () => {
-    let table = []
-
-    // Outer loop to create parent
-    for (let i = 0; i < 1; i++) {
-      const data = [1]
-      let children = []
-      //Inner loop to create children
-
-
-      children.push(<View style={{ flex: 1, alignItems: 'center' }}>
-
-        <Card containerStyle={{ width: 390, height: '100%' }} wrapperStyle={{}}>
-
-          <View style={{}}>
-            <View style={{ paddingLeft: 333 }}>
-              <Icon name="information-circle" style={styles.infoIcon} size={22} />
-            </View>
-            <Card.Title style={{ textAlign: 'left', fontSize: 20 }}>{global.users[i].titre_annonce}</Card.Title>
-            <ModalDropdown options={['Favoriser', 'Signaler']}>
-
-              <View style={styles.quizAttrRight}>
-                <View style={{ paddingLeft: 160 }}>
-
-                  <Text style={styles.infoText}></Text>
-                </View>
-              </View>
-
-            </ModalDropdown>
-          </View>
-
-
-          <View
-            style={{
-              position: "relative",
-            }}
-          >
-            <View style={{ alignItems: 'center', alignContent: 'center' }}>
-              <Image
-                style={{ width: 350, height: 200 }}
-                resizeMode="cover"
-                source={{
-                  uri:
-                    "https://i.ebayimg.com/images/g/1YgAAOSwy~JfzAGN/s-l300.jpg"
-                }}
-              />
-
-
-            </View>
-            <Card.Divider />
-
-            <View style={{}}>
-              <Text style={{ fontSize: 20, fontWeight: '700', color: 'red', textAlign: 'right' }}>{global.users[i].prix_annonce} EUR</Text>
-            </View>
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Catégorie : {global.users[i].categorie}</Text>
-
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Sous catégorie : {global.users[i].sous_categorie}</Text>
-
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Marque : {global.users[i].marque}</Text>
-
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Date : {global.users[i].date_publi_annonce}</Text>
-            <Card.Divider />
-            <Card.Title style={{ textAlign: 'left', fontSize: 20 }}>Information du vendeur</Card.Title>
-
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Nom :</Text>
-
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Prenom :</Text>
-
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Pays - Ville :</Text>
-
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Nombre d'annonces publiees :</Text>
-
-            <Text style={{ fontSize: 15, fontWeight: '700', borderBottomWidth: 15 }}>Moyens de contact :</Text>
-
-          </View>
-
-
-          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-            <Button style={{}}
-              color="green"
-              icon=""
-              mode="contained"
-              onPress={() => navigation.navigate('Annonce')}>
-              Contacter
-                                          </Button>
-            <View style={{ paddingLeft: 7 }}>
-              <Button style={{ alignItems: 'center', paddingLeft: 10, alignContent: 'center', height: 35, justifyContent: 'center' }}
-                color="yellow"
-                icon="star"
-                mode='contained'
-                onPress={() => alert("favoris")}>
-              </Button>
-            </View>
-          </View>
-
-
-
-        </Card>
-      </View>)
-
-
-      table.push(<View style={styles.container}>{children}</View>)
-    }
-    return table
-  }
-
-  async componentDidMount() {
-
-    // mettez  votre adresse IP
-    const url = "http://" + MON_IP + ":3000/utilisateur";
-
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({ person: data, loading: false });
-  }
-
-  render() {
-    if (this.state.loading) {
-      return (
-        <View style={[styles.container, styles.horizontal]}>
-          <ActivityIndicator size="large" color="tomato" />
-        </View>
-      );
-    }
-
-    if (!this.state.person) {
-      return <Text>Annonce introuvable :(</Text>;
-    }
-
-    return (
-
-      <View style={styles.container}>
-
-
-        <LinearGradient
-          colors={['#ffffff', '#c6f6ff', '#ff6347']}
-          style={styles.background}
-        >
-          <ScrollView >
-
-            <View>
-              {this.createTable()}
-            </View>
-
-          </ScrollView>
-
-          <View style={{ height: 177 }}>
-            {/* { navbar  } */}
-          </View>
-        </LinearGradient>
-      </View>
-
-    );
-  }
-}
 
 ////////////////////////////////////////////////////////////////////////////////
                             //DETAILS ANNONCE
@@ -540,7 +372,7 @@ export class DetailsAnnonce extends React.Component {
                               async componentDidMount() {
                             
                                 // mettez  votre adresse IP
-                                const url = "http://10.189.116.41:3000/utilisateur";
+                                const url = "http://10.212.156.25:3000/utilisateur";
                             
                                 const response = await fetch(url);
                                 const data = await response.json();
